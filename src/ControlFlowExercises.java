038ca24114930fd1cfa82712fd69c43f4d303c93
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-14 15:31:56 -0500

    completed # 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,30 @@
+import java.util.*;
+
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 0; i <101; i++){
-            if(i%15==0){
-                System.out.println("FizzBuzz");
-            }else
-            if(i%3==0){
-                System.out.println("Fizz");
-            }else
-            if (i%5==0){
-                System.out.println("Buzz");
-            }else {
+        int num;
+        int numSqrd;
+        int numCbd;
+        int inputNum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter a whole numbe");
+        inputNum = Integer.parseInt(sc.next());
+//        numSqrd = (int)Math.pow(num,2);
+//        numCbd = (int)Math.pow(num,3);
 
-                System.out.println(i);
-            }
+        String leftAlignFormat = " %-5s | %-7s |%-5s  %n";
+
+        System.out.format("-----------------+------%n");
+        System.out.format(" number| Squared | cubed   %n");
+        System.out.format("-----------------+------%n");
+        for (int i = 1; i <= inputNum; i++) {
+            num = i;
+            numSqrd = (int)Math.pow(num,2);
+            numCbd = (int)Math.pow(num,3);
+            System.out.format(leftAlignFormat,num, numSqrd,numCbd);
         }
+        System.out.format("+-----------------+------+%n");
+
     }
 }
