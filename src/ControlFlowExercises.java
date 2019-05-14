61a21dbc3e2f42f25db2ac8d01a587e77318d4a3
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-14 15:00:43 -0500

    fizzbuss exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,35 +1,18 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        int k = 0;
-        int j = 2;
-        int counter = 0;
-        while (i < 16) {
-            if (i == 15) {
+        for (int i = 0; i <101; i++){
+            if(i%15==0){
+                System.out.println("FizzBuzz");
+            }else
+            if(i%3==0){
+                System.out.println("Fizz");
+            }else
+            if (i%5==0){
+                System.out.println("Buzz");
+            }else {
+
                 System.out.println(i);
-                break;
             }
-            System.out.print(i++ + " ");
-        }
-
-        for(counter = 0;counter <3;) {
-            if (counter == 0) {
-                System.out.println(k++);
-                if (k == 101) {
-                    k = 100;
-                    counter = 1;
-                }
-            }
-            if (counter == 1) {
-                System.out.println(k -= 5);
-                if (k == -10) {
-                    counter = 3;
-                }
-            }
-        }
-
-        for(j=2;j<1000000;j = (int) Math.pow(j, 2)) {
-            System.out.println(j);
         }
     }
 }
