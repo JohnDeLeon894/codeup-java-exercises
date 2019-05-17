aac04fcfbe629cc94936f0eb797030eece5d13a9
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-17 11:40:10 -0500

    added person class

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+/*Object basics
+
+Create Person class inside of src that has a private name property that is a string, and the following methods:
+
+
+// returns the person's name
+public String getName();
+// changes the name property to the passed value
+public void setName(String name);
+// prints a message to the console using the person's name
+public void sayHello();
+The class should have a constructor that accepts a string value and sets the person's name to the passed string.
+
+Create a main method on the class that creates a new Person object and tests the above methods.
+*/
+
+
+
+public class Person {
+
+    private String name;
+
+    // returns the person's name
+    public String getName();
+    // changes the name property to the passed value
+    public void setName(String name);
+    // prints a message to the console using the person's name
+    public void sayHello();
+    The class should have a constructor that accepts a string value and sets the person's name to the passed string.
+
+
+}
